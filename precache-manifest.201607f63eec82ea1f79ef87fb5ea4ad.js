e16bfde4b1d229fadfcbcaf85aff09c5bab36d00
Avee <avi.pansara@gmail.com>
2019-03-06 15:22:39 -0600

    Updates

---
+++ precache-manifest.201607f63eec82ea1f79ef87fb5ea4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a243954e440ee49a07ec",
+    "url": "/payment-page/static/js/runtime~main.a243954e.js"
+  },
+  {
+    "revision": "e3165e34abff310379ef",
+    "url": "/payment-page/static/js/main.e3165e34.chunk.js"
+  },
+  {
+    "revision": "253480972146cadfb0da",
+    "url": "/payment-page/static/js/2.25348097.chunk.js"
+  },
+  {
+    "revision": "e3165e34abff310379ef",
+    "url": "/payment-page/static/css/main.712e984b.chunk.css"
+  },
+  {
+    "revision": "253480972146cadfb0da",
+    "url": "/payment-page/static/css/2.f30cd329.chunk.css"
+  },
+  {
+    "revision": "ffc46b6506ae90c17fac0d06ba612892",
+    "url": "/payment-page/index.html"
+  }
+];
